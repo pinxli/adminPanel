aab2bcad8fff7443a20bf8078a397dafd657b629
pinxli <pinkyliwanagan@yahoo.com>
2013-10-24 16:54:53 +0800

    apilogs

--- application/modules/admin/models/admin_model.php
+++ application/modules/admin/models/admin_model.php
@@ -7,6 +7,7 @@ class Admin_model extends CI_Model {
 	function validate($data)
 	{
 		$url = $this->config->item('rest_api_url') . 'users/login/'.$data['locale'].'/98740/';
+		// echo $url; exit;
 		return json_decode($this->call_rest_post($url,$data));
 	}
 	
